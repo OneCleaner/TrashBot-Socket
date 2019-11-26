85b59f7c44a832099af5aa09891a4420368ccc78
Gabriele Onorato <lapry34@gmail.com>
2019-11-26 16:08:48 +0100

    Update slave.ino

--- slave/slave.ino
+++ slave/slave.ino
@@ -7,7 +7,7 @@
 #include "Wire.h"
 #define BAUD 115200
 
-extern "C" { 
+extern "C" {
 #include "utility/twi.h"  // from Wire library, so we can do bus scanning
 }
 
@@ -16,8 +16,8 @@ extern "C" {
 
 SFEVL53L1X distanceSensor(Wire, -1, -1);
 
-#define pwm_a 6  //PWM control 
-#define pwm_b 9 
+#define pwm_a 6  //PWM control
+#define pwm_b 9
 #define pwm_c 5
 #define pwm_d 3
 
@@ -32,7 +32,7 @@ bool isMoving = false;
 #define dir_d 10 //Motore anteriore sinistro
 
 
-int distance[7] = {0};
+int distance[8] = {0};
 
 unsigned velocity = 0;
 
@@ -40,14 +40,14 @@ void setup(void){
 
   Serial.begin(BAUD);
   while(!Serial);
-  
+
   pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(pwm_a, OUTPUT);  //Set control pins to be outputs
   pinMode(pwm_b, OUTPUT);
   pinMode(pwm_c, OUTPUT);
   pinMode(pwm_d, OUTPUT);
-  
+
   pinMode(dir_a, OUTPUT);
   pinMode(dir_b, OUTPUT);
   pinMode(dir_c, OUTPUT);
@@ -59,19 +59,19 @@ void setup(void){
   //mettiamo così che parta in avanti alla prima
   digitalWrite(dir_a, HIGH);
   digitalWrite(dir_b, HIGH);
-  digitalWrite(dir_c, HIGH); 
+  digitalWrite(dir_c, HIGH);
   digitalWrite(dir_d, HIGH);
   digitalWrite(led_fari, HIGH);
   digitalWrite(led_freni, HIGH);
-  
+
   setVelocity(velocity);
 
   Wire.begin();
   delay(1000);
-  
+
   Serial.print("\nTCA ADDR: 0x"); Serial.println(TCA_ADDR, HEX);
   delay(1000);
-  for (uint8_t i = 0; i < 7; i++) {
+  for (uint8_t i = 0; i < 8; i++) {
     tcaselect(i);
     Serial.print("TCA Port #"); Serial.println(i);
     if (distanceSensor.begin() == 0) //Begin returns 0 on a good init
@@ -81,36 +81,38 @@ void setup(void){
 }
 
 void loop(void){
-  
+
   String data = "";
-  for (uint8_t i = 0; i < 7; i++) {
+  for (uint8_t i = 0; i < 8; i++) {
     tcaselect(i);
     distanceSensor.startRanging();
       distance[i] = distanceSensor.getDistance();
     distanceSensor.stopRanging();
+
   }
-  
-  if(distance[2] < 80 || distance[3] < 80) {
+
+  if(distance[2] < 500 || distance[3] < 500 || distance[7] < 1500) {
     setVelocity(0);
   } else if(isMoving) setVelocity(255);
 
   while(Serial.available() > 0){
     data = Serial.readString();
-  
+      
     if(data == "dritto"){
         digitalWrite(dir_a, HIGH);
         digitalWrite(dir_b, HIGH);
-        digitalWrite(dir_c, HIGH); 
+        digitalWrite(dir_c, HIGH);
         digitalWrite(dir_d, HIGH);
         digitalWrite(led_freni, LOW);
         setVelocity(velocity);
         Serial.println("Vado dritto");
+
     }
-    
+
     else if(data == "destra"){
         digitalWrite(dir_a, LOW);   //ruote di destra
         digitalWrite(dir_b, LOW);
-        digitalWrite(dir_c, HIGH); 
+        digitalWrite(dir_c, HIGH);
         digitalWrite(dir_d, HIGH);
         digitalWrite(led_freni, LOW);
         setVelocity(velocity);
@@ -119,33 +121,33 @@ void loop(void){
     }
 
     else if(data == "gira-destra"){
-        analogWrite(pwm_a, velocity -40);  
+        analogWrite(pwm_a, velocity -40);
         analogWrite(pwm_b, velocity -40);
         digitalWrite(dir_a, HIGH);   //ruote di destra
         digitalWrite(dir_b, HIGH);
-        digitalWrite(dir_c, HIGH); 
+        digitalWrite(dir_c, HIGH);
         digitalWrite(dir_d, HIGH);
         digitalWrite(led_freni, LOW);
         setVelocity(velocity);
         Serial.println("Giro a destra");
     }
-    
+
     else if(data == "sinistra"){
         digitalWrite(dir_a, HIGH);
         digitalWrite(dir_b, HIGH);
-        digitalWrite(dir_c, LOW); //ruote di sinistra  
+        digitalWrite(dir_c, LOW); //ruote di sinistra
         digitalWrite(dir_d, LOW);
-        digitalWrite(led_freni, LOW); 
+        digitalWrite(led_freni, LOW);
         setVelocity(velocity);
         Serial.println("Svolto a sinistra");
     }
 
     else if(data == "gira-sinistra"){
-        analogWrite(pwm_c, velocity -40);  
+        analogWrite(pwm_c, velocity -40);
         analogWrite(pwm_d, velocity -40);
         digitalWrite(dir_a, HIGH);   //ruote di destra
         digitalWrite(dir_b, HIGH);
-        digitalWrite(dir_c, LOW); 
+        digitalWrite(dir_c, LOW);
         digitalWrite(dir_d, LOW);
         digitalWrite(led_freni, LOW);
         setVelocity(velocity);
@@ -154,32 +156,33 @@ void loop(void){
     else if(data == "retro"){
         digitalWrite(dir_a, LOW);
         digitalWrite(dir_b, LOW);
-        digitalWrite(dir_c, LOW); //ruote di sinistra  
+        digitalWrite(dir_c, LOW); //ruote di sinistra
         digitalWrite(dir_d, LOW);
         digitalWrite(led_freni, HIGH);
         setVelocity(velocity);
         Serial.println("Torno indietro");
     }
-    
+
     else if(data == "spegni"){
         digitalWrite(led_freni, HIGH);
         setVelocity(0);
         isMoving = false;
         Serial.println("Spengo i motori");
     }
-    
+
     else if(data == "accendi"){
         digitalWrite(led_freni, LOW);
         setVelocity(255);
         isMoving = true;
         Serial.println("Accendo i motori");
-    } 
+    }
+
 
     else if(isNum(data) && data.toInt() > 0 && data.toInt() <= 255){
         setVelocity(data.toInt());
         Serial.println("cambiata velocità a: " + data);
     }
-    
+
     else {
       Serial.println("Comando non riconosciuto: " + data);
     }
@@ -191,11 +194,11 @@ void loop(void){
 void setVelocity(unsigned v){
 
   velocity = v;
-  
+
   if(v > 255)
     return;
 
-  analogWrite(pwm_a, v);  
+  analogWrite(pwm_a, v);
   analogWrite(pwm_b, v);  //minimo 190 o non partono wewo
   analogWrite(pwm_c, v);
   analogWrite(pwm_d, v);
@@ -214,11 +217,11 @@ bool isNum(String str){
 }
 
 void tcaselect(uint8_t i) {
-  if (i > 7) return;
-  
+  if (i > 8) return;
+
   Wire.beginTransmission(TCA_ADDR);
   Wire.write(1 << i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
 
   return;
 }
